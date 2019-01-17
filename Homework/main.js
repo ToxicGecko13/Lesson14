2272752f357459f141f0db6e538619071833c541
Carsten Ruhl <ToxicGecko13@Gmail.com>
2019-01-17 13:34:05 -0800

    Fourth Commit - .sort() adds and cleaned up lines

--- Homework/main.js
+++ Homework/main.js
@@ -64,90 +64,116 @@ let people = [{
 }
 ];
 
+
+// version1 using "fullName" array
 function exercise1() {
     // Return an array of the full names of people making over $100,000.
 
-    let filterAnswer = people.filter((person) => {
-        return person.Salary > 100000
-    });
+    let filterAnswer = people.filter((person) => person.Salary > 100000);
 
-    let mapAnswer = filterAnswer.map((person) => {
-        return person.fullName;
-    });
+    let mapAnswer = filterAnswer.map((person) => person.fullName);
 
     document.getElementById("1").innerText = `Answer: ${JSON.stringify(mapAnswer)}`; 
     
-
-    // // Return an array of the full names of people making over $100,000.
-    // let filterAnswer = people.filter((person) => {
-    //     return person.Salary > 100000
-    // });
-
-    // let mapAnswer1 = filterAnswer.map((person) => {
-    //     return person.firstName;
-    // });
-
-    // let mapAnswer2 = filterAnswer.map((person) => {
-    //     return person.lastName;
-    // });
-
-    // let names = mapAnswer1.concat(mapAnswer2);
-
-    // document.getElementById("1").innerText = `Answer: ${JSON.stringify(names)}`; 
-    
 }
 
+// // version using .concat and sort()
+// function exercise1() {
+//     // Return an array of the full names of people making over $100,000.
+
+//     let filterAnswer = people.filter((person) => person.Salary > 100000);
+
+//     let mapAnswer1 = filterAnswer.map((person) => person.firstName);
+
+//     let mapAnswer2 = filterAnswer.map((person) => person.lastName);
+
+//     let names = mapAnswer1.concat(mapAnswer2).sort();
+
+//     document.getElementById("1").innerText = `Answer: ${JSON.stringify(names)}`; 
+    
+// }
 
 
 function exercise2() {
     // Return a total cost of the team by adding up salaries
 
-    let allSalaries = people.reduce( (accumulator, person) => {
-        return accumulator + person.Salary;
-    }, 0 );
+    let finalAnswer = people.reduce( (accumulator, person) => accumulator + person.Salary, 0 );
 
-    document.getElementById("2").innerText = `Answer: ${JSON.stringify(allSalaries)}`;
+    document.getElementById("2").innerText = `Answer: ${JSON.stringify(finalAnswer)}`;
 
 }
 
 
-
+// version1 using "fullName" array
 function exercise3() {
     // Return an array of the full names of people with "SQL" skills
     // if you do a filter on person.Skills you can test for a specific skill.
 
-    let filterAnswer = people.filter((person) => {
-        return person.Skills === "SQL";
+    let filterAnswer = people.filter((person) => { 
+        let filterAn = person.Skills.filter(skill => skill === "SQL" ).length > 0;
+        return filterAn;
     });
 
-    let answer = filterAnswer.map((person) => {
-        return person.fullName;
-    });
+    let finalAnswer = filterAnswer.map((person) => person.fullName);
 
-    document.getElementById("3").innerText = `Answer: ${JSON.stringify(answer)}`;
+    document.getElementById("3").innerText = `Answer: ${JSON.stringify(finalAnswer)}`;
 
 }
 
+// // version using .concat and sort()
+// function exercise3() {
+//     // Return an array of the full names of people with "SQL" skills
+//     // if you do a filter on person.Skills you can test for a specific skill.
+
+//     let filterAnswer = people.filter((person) => { 
+//         let filterAn = person.Skills.filter(skill => skill === "SQL" ).length > 0;
+//         return filterAn;
+//     });
+
+//     let fNames = filterAnswer.map((person) => person.firstName);
+
+//     let lNames = filterAnswer.map((person) => person.lastName);
+
+//     let finalAnswer = fNames.concat(lNames).sort();
+
+//     document.getElementById("3").innerText = `Answer: ${JSON.stringify(finalAnswer)}`;
+
+// }
 
 
+// version1 using "fullName" array
 function exercise4() {
     // Return an array of people with "Software Engineer" in their job title.  Each object in the array you return should have 2 properties, fullName, and job.
     // you can use the method of .includes(substring) on a string to find if a string is found inside a string.
 
-    let softwareEngineer = people.filter((person) => {
-        return person.job.includes("Software Engineer");
-    });
+    let softwareEngineers = people.filter((person) => person.job.includes("Software Engineer"));
 
-    let peoplesNames = softwareEngineer.map((person) => {
-        return person.fullName;
-    });
+    let peoplesNames = softwareEngineers.map((person) => person.fullName);
 
-    let peoplesJobs = softwareEngineer.map((person) => {
-        return person.job;
-    });
+    let peoplesJobs = softwareEngineers.map((person) => person.job);
 
     let finalAnswer = peoplesNames.concat(peoplesJobs);
+    // let finalAnswer = peoplesNames.concat(peoplesJobs).sort();
 
     document.getElementById("4").innerText = `Answer: ${JSON.stringify(finalAnswer)}`;
 
 }
+
+// // version using .concat and sort()
+// function exercise4() {
+//     // Return an array of people with "Software Engineer" in their job title.  Each object in the array you return should have 2 properties, fullName, and job.
+//     // you can use the method of .includes(substring) on a string to find if a string is found inside a string.
+
+//     let softwareEngineers = people.filter((person) => person.job.includes("Software Engineer"));
+
+//     let peoplesFNames = softwareEngineers.map((person) => person.firstName);
+
+//     let peoplesLNames = softwareEngineers.map((person) => person.lastName);
+
+//     let peoplesJobs = softwareEngineers.map((person) => person.job);
+
+//     let finalAnswer = peoplesFNames.concat(peoplesLNames).concat(peoplesJobs).sort();
+
+//     document.getElementById("4").innerText = `Answer: ${JSON.stringify(finalAnswer)}`;
+
+// }
